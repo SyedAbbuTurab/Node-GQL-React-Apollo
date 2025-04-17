00d79ee35e1d9915d5f60f4d3bae149563a61102
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-16 21:29:28 -0500

    Updated server file to create a base setup connection and base query mutations

--- server/server.js
+++ server/server.js
@@ -1,15 +1,65 @@
 const server = require("express");
-const graphQlHttp = require("express-graphql");
+const { graphqlHTTP } = require('express-graphql');
 const bodyParser = require("body-parser");
+const { buildSchema } = require("graphql");
 const dotEnv = require("dotenv").config();
 
 const app = server();
 
-app.use(bodyParser.json());
-app.use('/graphql', graphQlHttp({}));
+let events = [];
 
-app.listen(process.env.PORT, ()=>{
+app.use(bodyParser.json());
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        type RootQuery {
+            events: [Event!]!
+        }
+        
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event    
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+        
+    `),
+    rootValue: {
+        events: () => {
+            return events;
+        },
+        createEvent: ({ eventInput }) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: eventInput.title,
+                description: eventInput.description,
+                price: +eventInput.price,
+                date: eventInput.date
+            }
+            events.push(event);
+            return event;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(process.env.PORT, () => {
     console.log(`Express GraphQl running on PORT:${process.env.PORT}`);
-    
+
 })
 
