464ed01e72947dc49d0db276fd1ff2d0acedfaeb
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-18 15:34:41 -0500

    Optimized the create user resolver service

--- server/server.js
+++ server/server.js
@@ -79,23 +79,32 @@ app.use('/graphql', graphqlHTTP({
                 throw err;
             })
         },
-        createUser: ({ userInput }) => {
-            return User.findOne({ email: userInput.email }).then(user => {
-                if (user) {
-                    throw new Error('User Already Exist!')
+        createUser: async ({ userInput }) => {
+            try {
+                const existingUser = await User.findOne({ email: userInput.email });
+                if (existingUser) {
+                    throw new Error('User Already Exists!');
                 }
-                return bcrypt.hash(userInput.password, 12)
-            }).then(async hashedPassword => {
+
+                const hashedPassword = await bcrypt.hash(userInput.password, 12);
+
                 const user = new User({
                     email: userInput.email,
                     password: hashedPassword
-                })
+                });
+
                 const result = await user.save();
-                return { ...result._doc, password: null };
-            }).catch(err => {
-                throw err
-            })
+
+                return {
+                    ...result._doc,
+                    password: null,
+                    _id: result.id
+                };
+            } catch (err) {
+                throw new Error(`User creation failed: ${err.message}`);
+            }
         }
+
     },
     graphiql: true
 }));
