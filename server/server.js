836204bd7a81f9b2c240108fbf16f18012b6f964
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-23 22:33:31 -0500

    event anduser relation managemnt

--- server/server.js
+++ server/server.js
@@ -12,9 +12,19 @@ const app = server();
 
 app.use(bodyParser.json());
 
+const events = (eventIds) => {
+    return Event.find({ _id: { $in: eventIds } }).then(events => {
+        return events.map(event => {
+            return { ...event._doc, _id: event.id, creator: user.bind(this, event.creator) }
+        })
+    }).catch(err => {
+        throw err
+    })
+}
+
 const user = (userId) => {
     return User.findById(userId).then(user => {
-        return { ...user._doc, _id: user.id }
+        return { ...user._doc, _id: user.id, createdEvents: events.bind(this, user._doc.createdEvents) }
     }).catch(err => { throw err })
 }
 app.use('/graphql', graphqlHTTP({
