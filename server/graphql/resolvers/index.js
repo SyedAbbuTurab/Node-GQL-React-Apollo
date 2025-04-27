3f18c7e9903016fba98d9e73e2c08ba4cf5081a8
Abbu Turab <syedabbuturab786@gmail.com>
2025-04-27 11:13:33 -0500

    Formatted to async functions

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -17,10 +17,13 @@ const events = async (eventIds) => {
     }
 }
 
-const user = (userId) => {
-    return User.findById(userId).then(user => {
+const user = async (userId) => {
+    try {
+        const user = await User.findById(userId)
         return { ...user._doc, _id: user.id, createdEvents: events.bind(this, user._doc.createdEvents) }
-    }).catch(err => { throw err })
+    } catch (err) {
+        throw err
+    }
 }
 
 module.exports = {
